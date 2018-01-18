0c546b9aed59eda33974df4269374b5324268bc6
shubhsingh594 <shubh.112358@gmail.com>
2018-01-19 01:30:56 +0530

    holiday_season

---
+++ tracksheet/holiday_season.cpp
@@ -0,0 +1,105 @@
+// template
+#include <string.h>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <complex>
+#include <list>
+
+using namespace std;
+
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz size()
+#define rep(i,m) for(int i=0; i<(int)(m); i++)
+#define rep2(i,n,m) for(int i=n; i<(int)(m); i++)
+#define For(it,c) for(__typeof(c.begin()) it=c.begin(); it!=c.end(); ++it)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define mp make_pair
+#define ff first
+#define ss second
+#define dot(a,b) ((conj(a)*(b)).X)
+#define X real()
+#define Y imag()
+#define length(V) (hypot((V).X,(V).Y))
+#define vect(a,b) ((b)-(a))
+#define cross(a,b) ((conj(a)*(b)).imag())
+#define normalize(v) ((v)/length(v))
+#define rotate(p,about,theta) ((p-about)*exp(point(0,theta))+about)
+#define pointEqu(a,b) (comp(a.X,b.X)==0 && comp(a.Y,b.Y)==0)
+#define mod 1000000007
+
+typedef stringstream ss;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+typedef vector<string> vs;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<vector<int> > vii;
+typedef long long ll;
+typedef long double ld;
+typedef complex<double> point;
+typedef pair<point, point> segment;
+typedef pair<double, point> circle;
+typedef vector<point> polygon;
+
+const int oo = (int) 1e9;
+const double PI = 2 * acos(0);
+const double eps = 1e-9;
+
+int acount[26][2001];
+vector<int>store[26];
+int main()
+{
+        int n;
+        string str;
+        cin>>n; int i;
+        cin>>str;
+        for(i=0; i<str.size(); i++)
+        {
+                char ch= str[i];
+                acount[ch-'a'][i]=1;
+                store[ch-'a'].pb(i);
+        }
+        int j;
+        for(i=0; i<26; i++)
+        {
+                for(j=1; j<=2000; j++)
+                {
+                        acount[i][j]=acount[i][j]+acount[i][j-1];
+                }
+        }
+        int t,k; ll result=0;
+        for(i=0; i<26; i++)
+        {
+                for(j=0; j<26; j++)
+                {
+                        for(t=0; t<store[i].size(); t++)
+                        {
+                                int x =store[i][t];
+                                if (x==0)
+                                        continue;
+                                for(k=t+1; k<store[i].size(); k++)
+                                {
+                                        int y=store[i][k];
+                                        result=result + (acount[j][x-1]*(acount[j][y-1]-acount[j][x]));
+                                }
+                        }
+                }
+        }
+        cout<<result<<"\n";
+        return 0;
+
+} //main
